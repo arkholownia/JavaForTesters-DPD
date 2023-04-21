2147fcd50a264afbcbfc716d3f4574f5a14cb610
arkholownia <arkadiusz.holownia-external@dpdgroup.com>
2023-04-21 10:51:10 +0200

    some changes

--- src/main/java/a_introduction/Task12.java
+++ src/main/java/a_introduction/Task12.java
@@ -1,7 +1,48 @@
 package a_introduction;
 
-public class Task12 {
-    public static void main(String[] args) {
-    }
+import java.text.DecimalFormat;
 
+public class Task12 {
+    static DecimalFormat df = new DecimalFormat("#");
+    public static void main(String[] args) {
+
+        System.out.println("środek ukłądu: " + getQuadrant(0,0));
+
+        System.out.println("ćwiartka: " + df.format(getQuadrant(1,1)));
+        System.out.println("ćwiartka: " + df.format(getQuadrant(1,-1)));
+        System.out.println("ćwiartka: " + df.format(getQuadrant(-1,-1)));
+        System.out.println("ćwiartka: " + df.format(getQuadrant(-1,1)));
+
+        System.out.println("prawa str: " + getQuadrant(1,0));
+        System.out.println("lewa  str: " + getQuadrant(-1,0));
+
+        System.out.println("góra: " + getQuadrant(0,1));
+        System.out.println("dół:  " + getQuadrant(0,-1));
+    }
+    public static double getQuadrant(double x, double y){
+
+        double quadrant = 0;
+
+        if (x > 0 && y > 0)
+            quadrant = 1;
+        if (x > 0 && y < 0)
+            quadrant = 2;
+        if (x < 0 && y < 0)
+            quadrant = 3;
+        if (x < 0 && y > 0)
+            quadrant = 4;
+
+        if (x == 0 && y > 0)
+            quadrant = 1.4;
+        if (x == 0 && y < 0)
+            quadrant = 2.3;
+
+        if (x > 0 && y == 0)
+            quadrant = 1.2;
+        if (x < 0 && y == 0)
+            quadrant = 3.4;
+
+
+        return quadrant;
+    }
 }
